1c87419c24eea897754a2022a7ec1519e56ffd1b
Karel Frajták <kfrajtak@lekis.cz>
2024-03-21 09:48:47 +0100

    @Component uncommented for AccountQueryEntityManager

--- src/main/java/com/progressivecoder/es/eventsourcingaxonspringboot/entities/handlers/AccountQueryEntityManager.java
+++ src/main/java/com/progressivecoder/es/eventsourcingaxonspringboot/entities/handlers/AccountQueryEntityManager.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 @Slf4j
 public class AccountQueryEntityManager {
 
